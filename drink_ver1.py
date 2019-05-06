831be3d938b933e168ae15b3119400be99c1eda1
raphaelseo22 <50270615+raphaelseo22@users.noreply.github.com>
2019-05-07 00:52:04 +0900

    Update drink_ver1.py

--- drink_ver1.py
+++ drink_ver1.py
@@ -2,9 +2,7 @@
 money = int(input('돈을 넣어주세요 : '))
 while True :
     left_money = '1'
-    if left_money == '2' :
-       money = money + int(input('돈을 넣어주세요 : '))
-    elif left_money == '0' : break
+    if left_money == '0' : break
     elif left_money == '1' :
         if money > 0 :
             print('1번 콜라 1000원  2번 환타 1300원  3번 파워에이드 1500원 4번 레쓰비 600원 5번 물 500원')
@@ -22,10 +20,10 @@ while True :
             if money < 0 :
                 print('금액이 부족합니다')
                 money = money + int(input('돈을 넣어주세요 : '))
-                left_money = input('주문을 그만 하시려면 0 더 하시려면 1 금액을 추가하고 싶으시면 2를 입력해주세요 : ')
-                if left_money == '0' : break
             left_money = input('주문을 그만 하시려면 0 더 하시려면 1 금액을 추가하고 싶으시면 2를 입력해주세요 : ')
             if left_money == '0' : break
+            if left_money == '2' :
+               money = money + int(input('돈을 넣어주세요 : '))
 
 print('잔돈은', money, '원입니다.')
 print('이용해주셔서 감사합니다')
